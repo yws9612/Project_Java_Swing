2600f05a93d695da94cc195421def479015a8552
서연운 <3yws@naver.com>
2022-06-21 20:59:37 +0900

    git test1

--- Java_Project_Swing/src/asdf.java
+++ Java_Project_Swing/src/asdf.java
@@ -3,7 +3,7 @@ public class asdf {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("git kraken test");
+		System.out.println("git a test");
 	}
 
 }
