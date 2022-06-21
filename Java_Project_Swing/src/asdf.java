124b074cc5dbe4462de5655a6e596ed351934dbf
서연운 <3yws@naver.com>
2022-06-21 21:00:36 +0900

    g t 1

--- Java_Project_Swing/src/asdf.java
+++ Java_Project_Swing/src/asdf.java
@@ -3,7 +3,7 @@ public class asdf {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("git a test");
+		System.out.println("git  test");
 	}
 
 }
