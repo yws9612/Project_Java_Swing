cbd8c31c6c705a4d7113cf0aa45fa0c2321abbf9
admoonistrator <91christian83@gmail.com>
2022-06-25 01:53:18 +0900

    랭킹페이지버튼_문종주_0625
    버튼라벨생성
    메인버튼클릭>선택페이지로이동

---
+++ Java_Project_Swing/src/RankingButton.java
@@ -0,0 +1,100 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.SwingConstants;
+
+public class RankingButton extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RankingButton frame = new RankingButton();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public RankingButton() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton bt_rk01 = new JButton("\uBA54\uC778\uC73C\uB85C");
+		bt_rk01.setBounds(25, 20, 150, 35);
+		contentPane.add(bt_rk01);
+		
+		JLabel lb_rk06 = new JLabel("\uC21C\uC704\uD45C");
+		lb_rk06.setHorizontalAlignment(SwingConstants.CENTER);
+		lb_rk06.setFont(new Font("±º∏≤", Font.PLAIN, 20));
+		lb_rk06.setBounds(375, 53, 150, 35);
+		contentPane.add(lb_rk06);
+		
+		JLabel lb_rk01 = new JLabel("1.");
+		lb_rk01.setHorizontalAlignment(SwingConstants.LEFT);
+		lb_rk01.setFont(new Font("±º∏≤", Font.PLAIN, 20));
+		lb_rk01.setBounds(300, 140, 300, 70);
+		contentPane.add(lb_rk01);
+		
+		JLabel lb_rk02 = new JLabel("2.");
+		lb_rk02.setHorizontalAlignment(SwingConstants.LEFT);
+		lb_rk02.setFont(new Font("±º∏≤", Font.PLAIN, 20));
+		lb_rk02.setBounds(300, 210, 300, 70);
+		contentPane.add(lb_rk02);
+		
+		JLabel lb_rk03 = new JLabel("3.");
+		lb_rk03.setHorizontalAlignment(SwingConstants.LEFT);
+		lb_rk03.setFont(new Font("±º∏≤", Font.PLAIN, 20));
+		lb_rk03.setBounds(300, 280, 300, 70);
+		contentPane.add(lb_rk03);
+		
+		JLabel lb_rk04 = new JLabel("4.");
+		lb_rk04.setHorizontalAlignment(SwingConstants.LEFT);
+		lb_rk04.setFont(new Font("±º∏≤", Font.PLAIN, 20));
+		lb_rk04.setBounds(300, 350, 300, 70);
+		contentPane.add(lb_rk04);
+		
+		JLabel lb_rk05 = new JLabel("5.");
+		lb_rk05.setHorizontalAlignment(SwingConstants.LEFT);
+		lb_rk05.setFont(new Font("±º∏≤", Font.PLAIN, 20));
+		lb_rk05.setBounds(300, 420, 300, 70);
+		contentPane.add(lb_rk05);
+		
+		bt_rk01.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new choice();
+                setVisible(false); // √¢ æ»∫∏¿Ã∞‘ «œ±‚ 
+            }
+        });
+		
+		setSize(900, 640);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		setVisible(true);
+	}
+}
--- Java_Project_Swing/src/choice.java
+++ Java_Project_Swing/src/choice.java
@@ -1,10 +1,15 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+
+
 
 public class choice extends JFrame {
 
@@ -35,25 +40,34 @@ public class choice extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton ch01Button = new JButton("\uC21C\uC6B0\uB9AC\uB9D0 \uB9DE\uCD94\uAE30");
-		ch01Button.setBounds(300, 140, 300, 70);
-		contentPane.add(ch01Button);
+		JButton bt_ch01 = new JButton("\uC21C\uC6B0\uB9AC\uB9D0 \uB9DE\uCD94\uAE30");
+		bt_ch01.setBounds(300, 140, 300, 70);
+		contentPane.add(bt_ch01);
 		
-		JButton ch02Button = new JButton("\uC21C\uD654\uC5B4 \uB9DE\uCD94\uAE30");
-		ch02Button.setBounds(300, 280, 300, 70);
-		contentPane.add(ch02Button);
+		JButton bt_ch02 = new JButton("\uC21C\uD654\uC5B4 \uB9DE\uCD94\uAE30");
+		bt_ch02.setBounds(300, 280, 300, 70);
+		contentPane.add(bt_ch02);
 		
-		JButton ch03Button = new JButton("\uB0B4\uB7AD\uD0B9");
-		ch03Button.setBounds(300, 420, 300, 70);
-		contentPane.add(ch03Button);
+		JButton bt_ch03 = new JButton("\uB0B4\uB7AD\uD0B9");
+		bt_ch03.setBounds(300, 420, 300, 70);
+		contentPane.add(bt_ch03);
 		
-		JButton ch03Button_1 = new JButton("\uB85C\uADF8\uC544\uC6C3");
-		ch03Button_1.setBounds(710, 542, 150, 35);
-		contentPane.add(ch03Button_1);
+		JButton bt_ch04 = new JButton("\uB85C\uADF8\uC544\uC6C3");
+		bt_ch04.setBounds(710, 542, 150, 35);
+		contentPane.add(bt_ch04);
+		
 		setSize(900, 640);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
 		setVisible(true);
+		
+		bt_ch03.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new RankingButton();
+                setVisible(false); // 창 안보이게 하기 
+            }
+        });
 	}
 }
