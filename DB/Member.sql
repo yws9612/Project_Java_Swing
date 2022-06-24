abe3dfde4daea75f279afad350bacff460416680
seoyoung <seoyoung9710@gmail.com>
2022-06-24 18:01:49 +0900

    MEMBER 시퀀스 생성 및 추가_서영윤경_0624
    MEMBER 시퀀스 생성(MEM_SQ)
    회원번호 시작 : 10000 끝 : 88888

--- DB/Member.sql
+++ DB/Member.sql
@@ -9,18 +9,30 @@ constraint u_email unique,
 ADMIN CHAR(1),
 constraint PK_M_NO PRIMARY KEY(M_NO));
 
-INSERT INTO MEMBER VALUES(99999,'admin','admin55','관리자','admin@gmail.com','Y');
-INSERT INTO MEMBER VALUES(10000,'wlxo','wlxo1','유지태','wlxo1@gmail.com','N');
-INSERT INTO MEMBER VALUES(10001,'dmsdn','dmsdn1','차은우','dmsdn1@gmail.com','N');
-INSERT INTO MEMBER VALUES(10002,'xodus','xodus1','김태연','xodus1@gmail.com','N');
-INSERT INTO MEMBER VALUES(10003,'dnjsdud','dnjsdud1','장원영','dnjsdud1@gmail.com','N');
-INSERT INTO MEMBER VALUES(10004,'wldms','wldms1','이지은','wldms1@gmail.com','N');
-INSERT INTO MEMBER VALUES(10005,'ehddnjs','ehddnjs1','강동원','ehddnjs1@gmail.com','N');
-INSERT INTO MEMBER VALUES(10006,'bin','bin1','원빈','bin1@gmail.com','N');
-INSERT INTO MEMBER VALUES(10007,'dusdns','dusdns1','서연운','dusdns1@gmail.com','N');
-INSERT INTO MEMBER VALUES(10008,'dlsrnr','dlsrnr1','서인국','dlsrnr1@gmail.com','N');
-INSERT INTO MEMBER VALUES(10009,'xogml','xogml1','김태희','xogml1@gmail.com','N');
-INSERT INTO MEMBER VALUES(10010,'dPtmf','dPtmf1','한예슬','dPtmf1@gmail.com','N');
+--MEMBER 시퀀스 생성
+CREATE SEQUENCE MEM_SQ
+INCREMENT BY 1
+START WITH 10000
+MAXVALUE 88888
+MINVALUE 10000
+NOCYCLE
+NOCACHE;
 
-SELECT * FROM MEMBER;
+--관리자 ID 삽입
+INSERT INTO MEMBER VALUES(99999,'admin','admin55','관리자','admin@gmail.com','Y');
+
+--회원 ID 삽입
+INSERT INTO MEMBER VALUES(MEM_SQ.NEXTVAL,'wlxo','wlxo1','유지태','wlxo1@gmail.com','N');
+INSERT INTO MEMBER VALUES(MEM_SQ.NEXTVAL,'dmsdn','dmsdn1','차은우','dmsdn1@gmail.com','N');
+INSERT INTO MEMBER VALUES(MEM_SQ.NEXTVAL,'xodus','xodus1','김태연','xodus1@gmail.com','N');
+INSERT INTO MEMBER VALUES(MEM_SQ.NEXTVAL,'dnjsdud','dnjsdud1','장원영','dnjsdud1@gmail.com','N');
+INSERT INTO MEMBER VALUES(MEM_SQ.NEXTVAL,'wldms','wldms1','이지은','wldms1@gmail.com','N');
+INSERT INTO MEMBER VALUES(MEM_SQ.NEXTVAL,'ehddnjs','ehddnjs1','강동원','ehddnjs1@gmail.com','N');
+INSERT INTO MEMBER VALUES(MEM_SQ.NEXTVAL,'bin','bin1','원빈','bin1@gmail.com','N');
+INSERT INTO MEMBER VALUES(MEM_SQ.NEXTVAL,'dusdns','dusdns1','서연운','dusdns1@gmail.com','N');
+INSERT INTO MEMBER VALUES(MEM_SQ.NEXTVAL,'dlsrnr','dlsrnr1','서인국','dlsrnr1@gmail.com','N');
+INSERT INTO MEMBER VALUES(MEM_SQ.NEXTVAL,'xogml','xogml1','김태희','xogml1@gmail.com','N');
+INSERT INTO MEMBER VALUES(MEM_SQ.NEXTVAL,'dPtmf','dPtmf1','한예슬','dPtmf1@gmail.com','N');
+INSERT INTO MEMBER VALUES(MEM_SQ.NEXTVAL,'xogud','xogud1','김태형','xogud1@gmail.com','N');
+
 
