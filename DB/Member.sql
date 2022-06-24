96b16d38bff1c6af6cfedeea94d3008ceddf1942
seoyoung <seoyoung9710@gmail.com>
2022-06-24 17:37:59 +0900

    MEMBER 회원추가완_서영윤경_0624
    M_NO : 10000 ~ 10010
    ID : 이름을 영타로 입력
    PW : ID + 1
    NAME : 연예인
    EMAIL : PW@gmail.com
    ADMIN : N

--- DB/Member.sql
+++ DB/Member.sql
@@ -10,9 +10,19 @@ ADMIN CHAR(1),
 constraint PK_M_NO PRIMARY KEY(M_NO));
 
 INSERT INTO MEMBER VALUES(99999,'admin','admin55','관리자','admin@gmail.com','Y');
+INSERT INTO MEMBER VALUES(10000,'wlxo','wlxo1','유지태','wlxo1@gmail.com','N');
+INSERT INTO MEMBER VALUES(10001,'dmsdn','dmsdn1','차은우','dmsdn1@gmail.com','N');
+INSERT INTO MEMBER VALUES(10002,'xodus','xodus1','김태연','xodus1@gmail.com','N');
+INSERT INTO MEMBER VALUES(10003,'dnjsdud','dnjsdud1','장원영','dnjsdud1@gmail.com','N');
+INSERT INTO MEMBER VALUES(10004,'wldms','wldms1','이지은','wldms1@gmail.com','N');
+INSERT INTO MEMBER VALUES(10005,'ehddnjs','ehddnjs1','강동원','ehddnjs1@gmail.com','N');
+INSERT INTO MEMBER VALUES(10006,'bin','bin1','원빈','bin1@gmail.com','N');
+INSERT INTO MEMBER VALUES(10007,'dusdns','dusdns1','서연운','dusdns1@gmail.com','N');
+INSERT INTO MEMBER VALUES(10008,'dlsrnr','dlsrnr1','서인국','dlsrnr1@gmail.com','N');
+INSERT INTO MEMBER VALUES(10009,'xogml','xogml1','김태희','xogml1@gmail.com','N');
+INSERT INTO MEMBER VALUES(10010,'dPtmf','dPtmf1','한예슬','dPtmf1@gmail.com','N');
+
 SELECT * FROM MEMBER;
-
-
 
 
 
