16a6ad4df51170747bbbed66618a48c53c82926c
SYW <3yws@naver.com>
2022-06-24 18:06:58 +0900

    OJDBC추가, 랭킹 클래스 업데이트_서연운_0624
    후에 classpath파일에서 classpathentry kind "lib" path 값을 ojdbc8.jar 로컬 위치로 바꿔줄것

--- Java_Project_Swing/src/Ranking.java
+++ Java_Project_Swing/src/Ranking.java
@@ -1,9 +1,66 @@
+import java.sql.*;
 
-public class Ranking {
+import javax.swing.*;
+import javax.swing.table.*;
 
+
+public class Ranking extends JPanel implements ActionListener{
+
+	JLabel mRanking;
+	JButton ranExit;
+	
+	JPanel rankPanel;
+	
+	
+	
+	
+	//랭킹 테이블 세팅
+	Object obj[][] = new Object[0][4];
+	DefaultTableModel model;
+	JTable rank;
+	JScrollPane jScroll;
+	String col [] = {"순위", "ID", "날짜", "점수"};//불러올 칼럼 이름
+	
+	//DB연동
+	Connection con = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
+	
+	
+	public Ranking(){
+		
+		rankPanel = new JPanel();
+		model = new DefaultTableModel(obj,col); //1)데이터 저장[][], 2)칼럼 이름
+		rank = new JTable(model);
+		jScroll = new JScrollPane(rank);
+		this.add(jScroll);
+		setBounds(250,250,300,300);
+		setVisible(true);
+		
+		//DB 접속 후 select 문장을 사용해 JTable에 보여주는 구문
+		connect();
+		select();
+		
+		this.addWindowListener(new WindowAdapter() {
+			
+			@Override
+			public void windowClosing(WindowEvent e) {
+				
+			}
+		});
+		
+		private void connect() {
+			try {
+				
+			}
+		}
+		
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new Ranking();
 	}
 
 }
