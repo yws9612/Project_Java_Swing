248dc4c93331f4ff491e0a783ff4c8d4a11b6c4b
서연운 <3yws@naver.com>
2022-06-25 14:45:01 +0900

    랭킹 코드 수정 및 추가_서연운_0625
    데이터베이스 연동중 순위 알고리즘이 애매해서 수정함

--- Java_Project_Swing/src/Ranking.java
+++ Java_Project_Swing/src/Ranking.java
@@ -1,10 +1,11 @@
+import java.awt.event.*;
 import java.sql.*;
 
 import javax.swing.*;
 import javax.swing.table.*;
 
 
-public class Ranking extends JPanel implements ActionListener{
+public class Ranking extends JFrame implements ActionListener{
 
 	JLabel mRanking;
 	JButton ranExit;
@@ -15,11 +16,11 @@ public class Ranking extends JPanel implements ActionListener{
 	
 	
 	//랭킹 테이블 세팅
-	Object obj[][] = new Object[0][4];
+	Object obj[][] = new Object[0][3];
 	DefaultTableModel model;
 	JTable rank;
 	JScrollPane jScroll;
-	String col [] = {"순위", "ID", "날짜", "점수"};//불러올 칼럼 이름
+	String col [] = {"ID", "날짜", "점수"};//불러올 칼럼 이름
 	
 	//DB연동
 	Connection con = null;
@@ -50,11 +51,14 @@ public class Ranking extends JPanel implements ActionListener{
 		});
 	}
 		
+		
+
 		private void connect() {
 			try {
+				//접속할 메모리 드라이버에 올림
 				Class.forName("oracle.jdbc.driver.OracleDriver");
 				String url = "jdbc:oracle:this@localhost:1521:xepdb1";
-				
+				//접속하기 위한 메소드(접속 url, 계정명, 암호)
 				con = DriverManager.getConnection(url, "SYW", "dusdns12!");
 				System.out.println("접속 : " + con);
 			}catch(Exception e) {
@@ -62,12 +66,40 @@ public class Ranking extends JPanel implements ActionListener{
 			}
 		}
 		
+		public void select() {
+			try {
+				String sql = "select id, score, playdate from member natural join score";
+				pstmt = con.prepareStatement(sql);
+				System.out.println("pstmt : " + pstmt);
+				rs = pstmt.executeQuery();
+				System.out.println("rs : " + rs);
+				
+				//테이블에서 값 불러오기
+				while(rs.next()) {
+					String id = rs.getString(1);
+					int score = rs.getInt(2);
+					java.sql.Date date = rs.getDate(3); 
+					
+				}
+			}
+		}
+
+
+
+
 		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Ranking();
 	}
+
+	
 
 }
