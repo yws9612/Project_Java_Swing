b865b4e7850554e3b5f7d8c12757c2d767b99856
nonameleel <xcvm92@gmail.com>
2022-06-26 01:26:02 +0900

    회원가입창_송이_0626
    회원가입창 배치 완료

--- Java_Project_Swing/src/sign_up.java
+++ Java_Project_Swing/src/sign_up.java
@@ -13,15 +13,15 @@ public class sign_up extends JFrame {
 	
 	String id, pw, pw2, name, email;
 	
-	TextField id_field=new TextField(30);
-	TextField pw_field=new TextField(30);
-	TextField pw2_field=new TextField(30);
-	TextField name_field=new TextField(30);
-	TextField email_field=new TextField(30);	
+	TextField id_field=new TextField();
+	TextField pw_field=new TextField();
+	TextField pw2_field=new TextField();
+	TextField name_field=new TextField();
+	TextField email_field=new TextField();	
 	
-	JButton id_check=new JButton("아이디 중복 확인");
+	JButton id_check=new JButton("아이디 확인");
 	JButton pw_check=new JButton("비밀번호 확인");
-	JButton email_check=new JButton("이메일 중복 확인");
+	JButton email_check=new JButton("이메일 확인");
 	JButton complete=new JButton("가입");
 	
 	JLabel title=new JLabel("회원가입");
@@ -36,26 +36,106 @@ public class sign_up extends JFrame {
 	JLabel last_checklabel=new JLabel();	
 	
 	sign_up() {
-		setLayout(new GridLayout(6,6));
-		add(title);
-		add(id_label);
-		add(id_field);
-		add(id_checklabel);
-		add(id_check);
-		add(pw_label);
-		add(pw_field);
-		add(pw2_label);
-		add(pw2_field);
-		add(pw_checklabel);
-		add(pw_check);
-		add(name_label);
-		add(name_field);
-		add(email_label);
-		add(email_field);
-		add(email_checklabel);
-		add(email_check);
-		add(complete);
-		add(last_checklabel);
+		Container c=getContentPane();
+		c.setLayout(null);
+		
+		title.setBackground(Color.ORANGE);
+		title.setOpaque(true);
+		title.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		title.setHorizontalAlignment(SwingConstants.CENTER);
+		title.setLocation(30, 25);
+		title.setSize(435, 40);
+		c.add(title);
+		
+		id_label.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		id_label.setLocation(30, 80);
+		id_label.setSize(100, 30);
+		c.add(id_label);
+		
+		id_field.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		id_field.setLocation(140, 80);
+		id_field.setSize(180, 30);
+		c.add(id_field);
+		
+		id_checklabel.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		id_checklabel.setLocation(140, 110);
+		id_checklabel.setSize(180, 30);
+		c.add(id_checklabel);
+		
+		id_check.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		id_check.setSize(125, 30);
+		id_check.setLocation(340, 80);
+		c.add(id_check);
+		
+		pw_label.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		pw_label.setLocation(30, 150);
+		pw_label.setSize(100, 30);
+		c.add(pw_label);
+		
+		pw_field.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		pw_field.setLocation(140, 150);
+		pw_field.setSize(180, 30);
+		c.add(pw_field);
+		
+		pw2_label.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		pw2_label.setLocation(30, 185);
+		pw2_label.setSize(100, 30);
+		c.add(pw2_label);
+		
+		pw2_field.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		pw2_field.setLocation(140, 185);
+		pw2_field.setSize(180, 30);
+		c.add(pw2_field);
+		
+		pw_checklabel.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		pw_checklabel.setLocation(140, 215);
+		pw_checklabel.setSize(180, 30);
+		c.add(pw_checklabel);
+		
+		pw_check.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		pw_check.setSize(125, 30);
+		pw_check.setLocation(340, 185);
+		c.add(pw_check);
+		
+		name_label.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		name_label.setLocation(30, 255);
+		name_label.setSize(100, 30);
+		c.add(name_label);
+		
+		name_field.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		name_field.setLocation(140, 255);
+		name_field.setSize(180, 30);
+		c.add(name_field);
+		
+		email_label.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		email_label.setLocation(30, 295);
+		email_label.setSize(100, 30);
+		c.add(email_label);
+		
+		email_field.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		email_field.setLocation(140, 295);
+		email_field.setSize(180, 30);
+		c.add(email_field);
+		
+		email_checklabel.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		email_checklabel.setLocation(140, 325);
+		email_checklabel.setSize(180, 30);
+		c.add(email_checklabel);
+		
+		email_check.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		email_check.setSize(125, 30);
+		email_check.setLocation(340, 295);
+		c.add(email_check);
+		
+		complete.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		complete.setSize(65, 30);
+		complete.setLocation(400, 365);
+		c.add(complete);
+		
+		last_checklabel.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		last_checklabel.setLocation(30, 365);
+		last_checklabel.setSize(350, 30);
+		c.add(last_checklabel);
 		
 		//비밀번호 확인 순서대로 확인 버튼, pw2_field에서 엔터, 탭
 		pw2_field.setFocusTraversalKeysEnabled(false);
@@ -64,7 +144,10 @@ public class sign_up extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				pw=pw_field.getText();
 				pw2=pw2_field.getText();
-				if(pw.equals(pw2)) {
+				if(pw.equals("")||pw2.equals("")) {
+					pw_checklabel.setText("비밀번호를 입력하세요.");
+				}
+				else if(pw.equals(pw2)) {
 					pw_checklabel.setText("확인");
 				}
 				else {
@@ -76,7 +159,10 @@ public class sign_up extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				pw=pw_field.getText();
 				pw2=pw2_field.getText();
-				if(pw.equals(pw2)) {
+				if(pw.equals("")||pw2.equals("")) {
+					pw_checklabel.setText("비밀번호를 입력하세요.");
+				}
+				else if(pw.equals(pw2)) {
 					pw_checklabel.setText("확인");
 				}
 				else {
@@ -89,12 +175,13 @@ public class sign_up extends JFrame {
 				if(e.getKeyCode()==KeyEvent.VK_TAB) {
 					pw=pw_field.getText();
 					pw2=pw2_field.getText().replace("\t", "");
-					if(pw.equals(pw2)) {
+					if(pw.equals("")||pw2.equals("")) {
+						pw_checklabel.setText("비밀번호를 입력하세요.");
+					}
+					else if(pw.equals(pw2)) {
 						pw_checklabel.setText("확인");
-						e.consume();
 					}
 					else {
-						e.consume();
 						pw_checklabel.setText("비밀번호가 다릅니다.");
 					}
 				}
@@ -104,20 +191,26 @@ public class sign_up extends JFrame {
 		//아이디 중복 확인
 		id_check.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				try {
-					String que="execute id_check";
-					psmt=conn.prepareStatement(que); //sql실행
-					boolean rs=psmt.execute();
-					if(rs) {
-						id_checklabel.setText
-							("이미 사용하고 있는 아이디입니다.");
-					}
-					else {
-						id_checklabel.setText
-							("사용할 수 있는 아이디입니다.");
-					}
-				} catch (Exception e_id_check) {
+				id=id_field.getText();
+				if(id.equals("")) {
+					id_checklabel.setText("아이디를 입력하세요.");
+				}
+				else {
+					try {
+						String que="execute id_check";
+						psmt=conn.prepareStatement(que); //sql실행
+						boolean rs=psmt.execute();
+						if(rs) {
+							id_checklabel.setText
+								("이미 사용하고 있는 아이디입니다.");
+						}
+						else {
+							id_checklabel.setText
+								("사용할 수 있는 아이디입니다.");
+						}
+					} catch (Exception e_id_check) {
 					
+					}
 				}
 			}
 		});
@@ -125,21 +218,29 @@ public class sign_up extends JFrame {
 		//이메일 중복 확인
 		email_check.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				try {
-				String que="execute email_check";
-				psmt=conn.prepareStatement(que); //sql실행
-				boolean rs=psmt.execute();
-				if(rs) {
+				email=email_field.getText();
+				if(email.equals("")) {
 					email_checklabel.setText
-						("이미 사용하고 있는 이메일입니다.");
+						("이메일을 입력하세요.");
 				}
 				else {
-					email_checklabel.setText
-						("사용할 수 있는 이메일입니다.");
-				}	
-				} catch (Exception e_email_check) {
-					
+					try {
+						String que="execute email_check";
+						psmt=conn.prepareStatement(que); //sql실행
+						boolean rs=psmt.execute();
+						if(rs) {
+							email_checklabel.setText
+								("이미 사용하고 있는 이메일입니다.");
+						}
+						else {
+							email_checklabel.setText
+								("사용할 수 있는 이메일입니다.");
+						}	
+					} catch (Exception e_email_check) {
+							
+					}
 				}
+				
 			}
 		});
 		
@@ -189,7 +290,7 @@ public class sign_up extends JFrame {
 			}
 		});
 		
-		setSize(500, 500);
+		setSize(510, 460);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
