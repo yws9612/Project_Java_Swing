e4286b5303644f50f3bf15a1e0fbc3f43aa77327
서연운 <3yws@naver.com>
2022-06-26 22:39:26 +0900

    트리거,프로시져 추가_서연운_0626
    트리거는 고쳐야하고 회원가입 프로시져 만듦

--- DB/Main_query.sql
+++ DB/Main_query.sql
@@ -4,9 +4,22 @@ create or replace trigger ch_score
 after 
 insert on score --score 테이블에 점수가 삽입됐을때
 begin
-    if (member.m_no = score.m_no) --멤버의 회원번호와 스코어의 회원번호가 일치할 때
-    then update score --score 테이블의 점수를
-    set score.score = (select max(score.score) from score where member.m_no = score.m_no);  --더 높은 점수로 업데이트
-    end if;
+    update score --score 테이블의 점수를
+    set score.score = (select max(score.score) from score where score.m_no);  --더 높은 점수로 업데이트/하려면 스코어 테이블이 하나 더있어야되나?
 end ch_score;
+/
+
+create or replace procedure reg_pr
+(
+    v_m_no in member.m_no % type,
+    v_id in member.id % type,
+    v_pw in member.pw % type,
+    v_name in member.name % type,
+    v_email in member.email % type
+)
+is
+begin
+    insert into member (m_no, id, pw, name, email, admin)
+    values(v_m_no, v_id, v_pw, v_name, v_email, null);
+end reg_pr;
 /
