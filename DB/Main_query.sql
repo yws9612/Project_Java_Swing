7b21a2052050f251fd9c0b1e68fc822a6af8a8a0
SYW <3yws@naver.com>
2022-06-27 10:58:18 +0900

    update_set 문장 수정_서_0627

--- DB/Main_query.sql
+++ DB/Main_query.sql
@@ -1,11 +1,12 @@
---트리거, 프로시져, 시퀀스 등 명령어는 보기 좋게 메인쿼리 브랜치에 올려주시면 될 것 같습니다! 
+ --트리거, 프로시져, 시퀀스 등 명령어는 보기 좋게 메인쿼리 브랜치에 올려주시면 될 것 같습니다! 
  
 create or replace trigger ch_score
 after 
 insert on score --score 테이블에 점수가 삽입됐을때
 begin
     update score --score 테이블의 점수를
-    set score.score = (select max(score.score) from score where score.m_no);  --더 높은 점수로 업데이트/하려면 스코어 테이블이 하나 더있어야되나?
+    set scores = max(scores)--최고점수로 갱신
+    where member.m_no = score.m_no;--where 절에서 본인임을 확인하는 식이 와야됨.
 end ch_score;
 /
 
