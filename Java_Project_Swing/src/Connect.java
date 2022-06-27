e6762ab8b8b71eec5798ea339cf737ba29229c3e
서연운 <3yws@naver.com>
2022-06-28 00:36:06 +0900

    DB코드 수정_서_0628
    conn is null 오류 해결 못함

--- Java_Project_Swing/src/Connect.java
+++ Java_Project_Swing/src/Connect.java
@@ -8,13 +8,13 @@ public class Connect {
 		Connection conn = null;
 		try {
 			// 접속할 메모리 드라이버에 올림
-			String id = "BBBB";
-			String pw = "1234";
-			String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+			String userId = "admin";
+			String pwd = "5groupDatabase";
+			String url = "jdbc:oracle:thin:@DB20220627204321_high?TNS_ADMIN=/D:/Wallet/Wallet_DB/Wallet_db20220627204321";//여기 파일경로는 바꿔야함
 
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			// 접속하기 위한 메소드(접속 url, 계정명, 암호)
-			con = DriverManager.getConnection(url, id, pw);
+			con = DriverManager.getConnection(url, userId, pwd);
 			// Connection객체를 얻음 ..conn은 실제로 데이터베이스와 연결하여
 			// 작업을 수행할 수 있는 통로로 작용하는 중요한 객체 변수로 사용됨
 
--- Java_Project_Swing/src/RankingButton.java
+++ Java_Project_Swing/src/RankingButton.java
@@ -22,7 +22,41 @@ public class RankingButton extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+
+		Connection conn = null; // DB와 연결하는 인터페이스
+
+		PreparedStatement psmt = null; // sql문 객체
+
+		ResultSet rs = null; // sql에 대한 반환(쿼리 실행에 대한 결과값 저장)
+
+		try {
+			String que = "select * from member";
+			conn = Connect.get(); // DB연결
+			psmt = conn.prepareStatement(que);
+			rs = psmt.executeQuery();
+
+			// select => executeQuery()
+
+			// DML(insert,update,delete) => executeUpdate();
+			while (rs.next()) {
+				String ID = rs.getString(2);
+				System.out.println(ID);
+				// DB에 있는 값들을 가져옴
+			}
+
+		}
+
+		catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+
+		rs.close();
+		psmt.close();
+		conn.close();
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -34,10 +68,7 @@ public class RankingButton extends JFrame {
 			}
 		});
 	}
-	
-	public static Connection con;
-	public static PreparedStatement pstmt;
-	public static ResultSet rs;
+
 
 	/**
 	 * Create the frame.
@@ -45,129 +76,66 @@ public class RankingButton extends JFrame {
 	public RankingButton() {
 //		connect();
 //		select();
-		
+
 		Link link = new Link();
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JButton bt_rk01 = new JButton("\uBA54\uC778\uC73C\uB85C");
 		bt_rk01.setBounds(25, 20, 150, 35);
 		contentPane.add(bt_rk01);
-		
+
 		JLabel lb_rk06 = new JLabel("\uC21C\uC704\uD45C");
 		lb_rk06.setHorizontalAlignment(SwingConstants.CENTER);
 		lb_rk06.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
 		lb_rk06.setBounds(375, 53, 150, 35);
 		contentPane.add(lb_rk06);
-		
+
 		JLabel lb_rk01 = new JLabel("1.");
 		lb_rk01.setHorizontalAlignment(SwingConstants.LEFT);
 		lb_rk01.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
 		lb_rk01.setBounds(300, 140, 300, 70);
 		contentPane.add(lb_rk01);
-		
+
 		JLabel lb_rk02 = new JLabel("2.");
 		lb_rk02.setHorizontalAlignment(SwingConstants.LEFT);
 		lb_rk02.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
 		lb_rk02.setBounds(300, 210, 300, 70);
 		contentPane.add(lb_rk02);
-		
+
 		JLabel lb_rk03 = new JLabel("3.");
 		lb_rk03.setHorizontalAlignment(SwingConstants.LEFT);
 		lb_rk03.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
 		lb_rk03.setBounds(300, 280, 300, 70);
 		contentPane.add(lb_rk03);
-		
+
 		JLabel lb_rk04 = new JLabel("4.");
 		lb_rk04.setHorizontalAlignment(SwingConstants.LEFT);
 		lb_rk04.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
 		lb_rk04.setBounds(300, 350, 300, 70);
 		contentPane.add(lb_rk04);
-		
+
 		JLabel lb_rk05 = new JLabel("5.");
 		lb_rk05.setHorizontalAlignment(SwingConstants.LEFT);
 		lb_rk05.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
 		lb_rk05.setBounds(300, 420, 300, 70);
 		contentPane.add(lb_rk05);
-		
+
 		bt_rk01.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Choice();
-                dispose(); // 창 안보이게 하기 
-            }
-        });
-		
-		
-		
-		//DB 접속 후 select 문장을 사용해 출력하는 구문
-	
-		
-		this.addWindowListener(new WindowListener() {
-			
-			
-
 			@Override
-			public void windowOpened(WindowEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void windowClosing(WindowEvent e) {
-				// TODO Auto-generated method stub
-				try {//프로그램 종료 전에 메모리에 DB연동 부분 해제
-					if(rs!= null) {
-						rs.close();
-					}
-					if(pstmt != null) {
-						pstmt.close();
-					}
-					if(con != null) {
-						con.close();
-					}
-			}catch(Exception e1) {
-				System.exit(0);
-				}
-			}
-
-			@Override
-			public void windowClosed(WindowEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void windowIconified(WindowEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void windowDeiconified(WindowEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void windowActivated(WindowEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void windowDeactivated(WindowEvent e) {
-				// TODO Auto-generated method stub
-				
+			public void actionPerformed(ActionEvent e) {
+				new Choice();
+				dispose(); // 창 안보이게 하기
 			}
 		});
-	
-		
+
+		// DB 접속 후 select 문장을 사용해 출력하는 구문
+
 		setSize(900, 640);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -186,10 +154,33 @@ public class RankingButton extends JFrame {
 //			System.out.println("DB접속 오류 : " + e);
 //		}
 //	}
-	
+
 //	public void select() {
 //		  
 //		
 //	}
+//	public void select() {
+//		Connection conn=null; //DB와 연결하는 인터페이스
+//
+//		PreparedStatement psmt=null; //sql문 객체
+//
+//        ResultSet rs=null; //sql에 대한 반환(쿼리 실행에 대한 결과값 저장)
+//        
+//        
+//        try {
+//			String sql = "select * from member";
+//			
+//			conn = Connect.get();
+//			psmt = conn.prepareStatement(sql);
+//			rs = psmt.executeQuery();
+//			
+//			//테이블에서 값 불러오기
+//			while(rs.next()) {
+//				String ID = rs.getString(2);
+//				System.out.println(ID);
+//			}
+//		}catch(Exception e) {
+//			System.out.println("select() 실행 오류 : " + e);
+//		}
+//	}
 }
-
