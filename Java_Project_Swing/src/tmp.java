4781126533863c55c29d8750ba4cc5389475c285
nonameleel <xcvm92@gmail.com>
2022-06-27 17:30:42 +0900

    게임부(임시파일)_송이_0627
    게임 글자 입력 부분 일부 작성

--- Java_Project_Swing/src/tmp.java
+++ Java_Project_Swing/src/tmp.java
@@ -1,4 +1,9 @@
+import java.awt.GridLayout;
 import java.sql.*;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 class thread extends Thread {
 	public void run() {
@@ -29,6 +34,8 @@ public class tmp {
 		t.start();
 		
 		//랜덤으로 단어 가져와서 정답 비교를 위해 배열로 쪼개기
+		String getword1;
+		char [] word1;
 		try {
 			conn=connecttoDB.get();
 			String que1="select * from word1";
@@ -42,14 +49,34 @@ public class tmp {
 			psmt=conn.prepareStatement(que2);
 			psmt.setInt(1,word1num);
 			rs=psmt.executeQuery();
-			String getword1=rs.getString(1);
-			char [] word1=new char[getword1.length()];
+			getword1=rs.getString(1);
+			word1=new char[getword1.length()];
 			for(int i=0; i<getword1.length(); i++) {
 				word1[i]=getword1.charAt(i);
-			}
-			
+			}			
 		} catch(Exception e) {
 			
+		}
+		
+		//게임부분 라벨/인풋칸 생성
+		int w_length=getword1.length();
+		
+		JFrame tmp=new JFrame();
+		tmp.setLayout(new GridLayout(w_length,5));
+		
+		JLabel [] a=new JLabel[w_length];
+		JLabel [] b=new JLabel[w_length];
+		JLabel [] c=new JLabel[w_length];
+		JLabel [] d=new JLabel[w_length];
+		JLabel [] e=new JLabel[w_length];
+		JTextField [] input=new JTextField[w_length];
+		
+		for(int i=0; i<w_length; i++) {
+			tmp.add(input[i]);
+			tmp.add(b[i]);
+			tmp.add(c[i]);
+			tmp.add(d[i]);
+			tmp.add(e[i]);
 		}
 		
 
