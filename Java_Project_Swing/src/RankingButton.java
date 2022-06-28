73c2c33afbae7480cb4c0255baffdb66337a7b26
SYW <3yws@naver.com>
2022-06-28 11:07:30 +0900

    DB연동 함수 구현 완료_서연운_0628
    연동시 필요한 파일들이 있음. https://okky.kr/article/970368 참고 하여 ojdbc 8 풀패키지 받은 후  파일 내부에있는 ojdbc8, cert, core, oraclepki 파일 buildpath로 프로젝트에 넣어줄것!

--- Java_Project_Swing/src/RankingButton.java
+++ Java_Project_Swing/src/RankingButton.java
@@ -24,38 +24,38 @@ public class RankingButton extends JFrame {
 	 */
 	public static void main(String[] args) throws SQLException {
 
-		Connection conn = null; // DB와 연결하는 인터페이스
-
-		PreparedStatement psmt = null; // sql문 객체
-
-		ResultSet rs = null; // sql에 대한 반환(쿼리 실행에 대한 결과값 저장)
-
-		try {
-			String que = "select * from member";
-			conn = Connect.get(); // DB연결
-			psmt = conn.prepareStatement(que);
-			rs = psmt.executeQuery();
-
-			// select => executeQuery()
-
-			// DML(insert,update,delete) => executeUpdate();
-			while (rs.next()) {
-				String ID = rs.getString(2);
-				System.out.println(ID);
-				// DB에 있는 값들을 가져옴
-			}
-
-		}
-
-		catch (Exception e) {
-
-			e.printStackTrace();
-
-		}
-
-		rs.close();
-		psmt.close();
-		conn.close();
+//		Connection conn = null; // DB와 연결하는 인터페이스
+//
+//		PreparedStatement psmt = null; // sql문 객체
+//
+//		ResultSet rs = null; // sql에 대한 반환(쿼리 실행에 대한 결과값 저장)
+//
+//		try {
+//			String que = "select * from member";
+//			conn = Connect.get(); // DB연결
+//			psmt = conn.prepareStatement(que);
+//			rs = psmt.executeQuery();
+//
+//			// select => executeQuery()
+//
+//			// DML(insert,update,delete) => executeUpdate();
+//			while (rs.next()) {
+//				String ID = rs.getString(2);
+//				System.out.println(ID);
+//				// DB에 있는 값들을 가져옴
+//			}
+//
+//		}
+//
+//		catch (Exception e) {
+//
+//			e.printStackTrace();
+//
+//		}
+//
+//		rs.close();
+//		psmt.close();
+//		conn.close();
 
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -75,7 +75,7 @@ public class RankingButton extends JFrame {
 	 */
 	public RankingButton() {
 //		connect();
-//		select();
+		select();
 
 		Link link = new Link();
 
@@ -159,28 +159,28 @@ public class RankingButton extends JFrame {
 //		  
 //		
 //	}
-//	public void select() {
-//		Connection conn=null; //DB와 연결하는 인터페이스
-//
-//		PreparedStatement psmt=null; //sql문 객체
-//
-//        ResultSet rs=null; //sql에 대한 반환(쿼리 실행에 대한 결과값 저장)
-//        
-//        
-//        try {
-//			String sql = "select * from member";
-//			
-//			conn = Connect.get();
-//			psmt = conn.prepareStatement(sql);
-//			rs = psmt.executeQuery();
-//			
-//			//테이블에서 값 불러오기
-//			while(rs.next()) {
-//				String ID = rs.getString(2);
-//				System.out.println(ID);
-//			}
-//		}catch(Exception e) {
-//			System.out.println("select() 실행 오류 : " + e);
-//		}
-//	}
+	public void select() {
+		Connection conn=null; //DB와 연결하는 인터페이스
+
+		PreparedStatement psmt=null; //sql문 객체
+
+        ResultSet rs=null; //sql에 대한 반환(쿼리 실행에 대한 결과값 저장)
+        
+        
+        try {
+			String sql = "select * from member";
+			
+			conn = Connect.get();
+			psmt = conn.prepareStatement(sql);
+			rs = psmt.executeQuery();
+			
+			//테이블에서 값 불러오기
+			while(rs.next()) {
+				String ID = rs.getString(2);
+				System.out.println(ID);
+			}
+		}catch(Exception e) {
+			System.out.println("select() 실행 오류 : " + e);
+		}
+	}
 }
