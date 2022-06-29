43dfff8512a7a94dfa8229f6196ea13fa18eba18
nonameleel <xcvm92@gmail.com>
2022-06-29 18:43:51 +0900

    게임부_송이용학윤경_0629
    게임부 텍스트필드 배치 작성 중

--- Java_Project_Swing/src/game1.java
+++ Java_Project_Swing/src/game1.java
@@ -1,3 +1,4 @@
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -6,28 +7,37 @@ import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class game1 {
-
-	public static void main(String[] args) {
-		String getword1 = null;
+public class game1 extends JPanel {
+	JLabel [] a=new JLabel[6];
+	JLabel [] b=new JLabel[6];
+	JLabel [] c=new JLabel[6];
+	JLabel [] d=new JLabel[6];
+	JLabel [] e=new JLabel[6];;
+	JTextField [] input_field=new JTextField[6];
+	char [] input;
+	boolean [] grading;
+	int w_length;
+	
+	public void set_grading(boolean [] boo) {
+		for(int i=0; i<w_length; i++) {
+			grading[i]=boo[i];
+		}
+	}
+	public void set_length(int w_length) {
+		this.w_length=w_length;
+	}	
+	
+	game1() {
+		setLayout(new GridLayout(w_length,5));		
 		
-		int w_length=getword1.length();
-		
-		JFrame tmp=new JFrame();
-		tmp.setLayout(new GridLayout(w_length,5));
-		
-		JLabel [] a=new JLabel[w_length];
-		JLabel [] b=new JLabel[w_length];
-		JLabel [] c=new JLabel[w_length];
-		JLabel [] d=new JLabel[w_length];
-		JLabel [] e=new JLabel[w_length];
-		JTextField [] input_field=new JTextField[6];
-		char [] input=new char[w_length];
-		boolean [] grading=new boolean[w_length];
+		input=new char[w_length];
+		grading=new boolean[w_length];
 
 		// textfield 글자수 제한+자동포커스 이동
+		/*
 		input_field[0].addKeyListener(new KeyAdapter() {
 			public void keyTyped(KeyEvent ke) {
 				JTextField src = (JTextField) ke.getSource();
@@ -111,7 +121,7 @@ public class game1 {
 		});
 		input_field[5].addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//채점 기준으로 넘어가야 한다.
+				//채점으로 넘어가야 한다.
 			}
 		});
 		
@@ -119,18 +129,19 @@ public class game1 {
 		input_field[5].addKeyListener(new KeyAdapter() {
 			public void keyPressed(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_TAB) {
-					//채점 기준으로 넘어가야 한다.
+					//채점으로 넘어가야 한다.
 				}
 			}
 		});
 		
-		//단어 끝칸
+		//단어 끝칸_엔터
 		input_field[w_length-1].addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//마지막 칸 엔터쳤을 때 이벤트 > 채점+다음 시도로 넘기기				
+				//채점+다음 시도로 넘기기				
 			}
 		});
 		
+		//단어 끝칸_탭
 		input_field[w_length-1].setFocusTraversalKeysEnabled(false);
 		input_field[w_length-1].addKeyListener(new KeyAdapter() {
 			public void keyPressed(KeyEvent e) {
@@ -139,6 +150,35 @@ public class game1 {
 				}
 			}
 		});
+		*/
+		
+		
+		
+		for(int i=0; i<w_length; i++) {
+			input_field[i]=new JTextField();
+			add(input_field[i]);
+			add(b[i]);
+			add(c[i]);
+			add(d[i]);
+			add(e[i]);
+		}
+		
+		setSize(w_length*30, (30*5)+(10*4));
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		
+		JFrame tmp=new JFrame();
+		JPanel tmp2=new game1();
+		game1 g1=new game1();
+		g1.set_length(5);
+		tmp.setLayout(new BorderLayout());
+		tmp.add(tmp2,BorderLayout.CENTER);
+		tmp.setSize(700, 700);
+		tmp.setVisible(true);
+		tmp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
 
 }
