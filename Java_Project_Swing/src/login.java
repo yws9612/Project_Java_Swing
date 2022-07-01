2a2e3df666008f5d60dc2327f2ec85b8b5fa8fe4
SYW <3yws@naver.com>
2022-07-01 17:44:41 +0900

    로그인 수정

--- Java_Project_Swing/src/login.java
+++ Java_Project_Swing/src/login.java
@@ -1,76 +1,185 @@
-import java.awt.TextField;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.HashMap;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.*;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.UIManager;
 
-class log_in {
-	HashMap<String, String> log_in=new HashMap<String, String>();
-	String id, pw;
-	log_in(TextField tf_id, TextField tf_pw){
-		id=tf_id.getText();
-		pw=tf_pw.getText();
-		if(log_in.containsKey(id)) {
-			if(pw.equals(log_in.get(id))) {
-				//메뉴화면으로 넘어갑니다
-			}
-			else {
-				//아이디 비번 확인하라는 라벨이 붙습니다.
-			}
-		}
+
+class Getter{
+	private String result;
+	private String admin;
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+	public String getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(String admin) {
+		this.admin = admin;
 	}
 }
+public class login extends JFrame {
+	Connection conn=null; 
+	PreparedStatement psmt=null;
+	ResultSet rs=null;
+	static String id,pw;
 
-class search_id {
-	HashMap<String, String> srch_id=new HashMap<String, String>();
-	String name, email;
-	search_id(TextField tf_name, TextField tf_email){
-		name=tf_name.getText();
-		email=tf_email.getText();
-		String srch=name+email;
-		if(srch_id.containsKey(name)) {
-			if(email.equals(srch_id.get(name))) {
-				//아이디를 보여줍니다. -- 아이디 찾을 방법이 없습니다.
+	JLabel title = new JLabel("5dle,5dle");
+	JLabel id_label = new JLabel("ID");
+	JLabel pw_label = new JLabel("PW");
+	JLabel alter = new JLabel();
+	
+	JTextField id_field = new JTextField();
+	JPasswordField pw_field= new JPasswordField();
+	
+	JButton sign_up = new JButton("회원가입");
+	JButton log_in = new JButton("로그인");
+	JButton find_id = new JButton("아이디 찾기");
+	JButton find_pw = new JButton("비밀번호 찾기");
+	
+	
+	login() {
+		setBackground(Color.WHITE);
+		setLayout(null);
+		title.setForeground(UIManager.getColor("Button.focus"));
+		title.setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+		title.setHorizontalAlignment(SwingConstants.CENTER);
+		title.setBackground(Color.GRAY);
+		title.setSize(331, 113);
+		title.setLocation(0, 0);
+			
+		add(title);
+		id_label.setHorizontalAlignment(SwingConstants.CENTER);
+		id_label.setSize(64, 40);
+		id_label.setLocation(10, 170);
+		add(id_label);
+		pw_label.setHorizontalAlignment(SwingConstants.CENTER);
+		pw_label.setLocation(10, 220);
+		pw_label.setSize(64, 40);
+		add(pw_label);
+		id_field.setLocation(86, 171);
+		id_field.setSize(233, 40);
+		
+		add(id_field);
+		pw_field.setLocation(86, 221);
+		pw_field.setSize(233, 40);
+		add(pw_field);
+		sign_up.setBackground(Color.WHITE);
+		sign_up.setLocation(10, 270);
+		sign_up.setSize(150, 40);
+		
+		add(sign_up);
+		log_in.setLocation(172, 271);
+		log_in.setSize(150, 40);
+		add(log_in);
+		find_id.setLocation(10, 320);
+		find_id.setSize(150, 40);
+		add(find_id);
+		find_pw.setLocation(172, 320);
+		find_pw.setSize(150, 40);
+		add(find_pw);
+		
+		alter.setSize(283, 40);
+		alter.setLocation(36, 123);		
+		add(alter);
+			
+		sign_up.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new sign_up();
 			}
-			else {
-				//아이디가 없습니다.
+		});
+		
+		log_in.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				id = id_field.getText();
+				pw = pw_field.getText();
+				
+				if(id.equals("")||pw.equals("")) {
+					alter.setText("id 혹은 비밀번호를 확인하세요.");					
+				}
+				else {
+					try {
+						String que="select pw, admin from member where id=?";
+						conn=Connect.get();
+						psmt=conn.prepareStatement(que);
+						psmt.setString(1, id);
+						rs=psmt.executeQuery();
+						Getter get = new Getter();
+						while(rs.next()) {
+							get.setResult(rs.getString(1));
+							get.setAdmin(rs.getString(2));
+						}
+						
+						if(pw.equals(get.getResult())) {
+							if(get.getAdmin().equals("Y")) {
+//								new ManagerChoice();
+								dispose();
+							}else {
+								new Choice();
+								dispose();
+							}
+						}else {
+							alter.setText("id 혹은 비밀번호를 확인하세요.");
+						}
+						
+					} catch(Exception e_compare) {
+						System.out.println("e_compare_error");
+					}
+					
+				}
 			}
-		}
+		});
+		
+		find_id.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new find_id();
+			}
+		});
+		
+		find_pw.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new find_pw();
+			}
+		});
+		
+		setSize(800,800);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-}
-
-class search_pw {
-	HashMap<String, String> srch_pw=new HashMap<String, String>();
-	String id;
-	search_pw(TextField tf_id){
-		id=tf_id.getText();
-		if(srch_pw.containsKey(id)) {
-			String pw=srch_pw.get(id);
-			//비번를 보여줍니다.
-		}
-		else {
-			//아이디가 없습니다.
-		}
-	}
-}
-
-public class login {
 
 	public static void main(String[] args) {
-		Connection conn=null; //DB와 연결하는 인터페이스
-		PreparedStatement psmt=null; //sql문 객체
-		ResultSet rs=null; //sql에 대한 반환(쿼리 실행에 대한 결과값 저장)
+		
+		/* 참고용 DB에서 내용 추출 코드
+		Connection conn=null; 
+		PreparedStatement psmt=null; 
+		ResultSet rs=null; 
 		try {
-			String que="select * from emp1";
-			conn=A1.get(); //DB연결
-			psmt=conn.prepareStatement(que); //sql실행
+			String que="";
+			conn=ConnecttoDB.get(); 
+			psmt=conn.prepareStatement(que); 
 			rs=psmt.executeQuery();
-			/*select -> executeQuery()
-			DML(insert, update, delete) -> executeUpdate(); */
+			/select -> executeQuery()
+			DML(insert, update, delete) -> executeUpdate(); /
 			
-			/*
 			while(rs.next()) {
-				//DB에 있는 값을 가져온다.
 				String empno=rs.getString(1);
 				String ename=rs.getString(2);
 				String job=rs.getString(3);
@@ -80,14 +189,18 @@ public class login {
 				int comm=rs.getInt(7);
 				int deptno=rs.getInt(8);
 				System.out.println(empno+" "+ename+" "+hiredate);
-				*/
 			}
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 		rs.close();
 		psmt.close();
-		conn.close();​
+		conn.close();
+		*/
+		
+		
+		//테스트용 임시프레임
+		JFrame log_in=new login();
 		
 
 	}
