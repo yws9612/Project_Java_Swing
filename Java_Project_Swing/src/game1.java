ffc2e55d6467b8633b815a2c1c0bcc59cb6d5821
nonameleel <xcvm92@gmail.com>
2022-07-03 14:19:10 +0900

    게임부_송이_0703
    계속 깨알 조정 중ㅎㅎ;;

--- Java_Project_Swing/src/game1.java
+++ Java_Project_Swing/src/game1.java
@@ -1,6 +1,5 @@
 import java.awt.*;
 import java.awt.event.*;
-
 import javax.swing.*;
 
 public class game1 extends JPanel {
@@ -25,163 +24,167 @@ public class game1 extends JPanel {
 		t.start();
 		try {
 			t.join();
-		} catch(Exception e) {
+
+			w_length = a;
+			setLayout(new GridLayout(5, w_length,10,10));
+
+			input1 = new char[w_length];
+			grading = new boolean[w_length];
+
+			for (int i = 0; i < 6; i++) {
+				input_field[i] = new JTextField();
+			}
+
+			for (int i = 0; i < w_length; i++) {
+				input_field[i].setFont(new Font("DungGeunMo", Font.PLAIN, 40));
+				input_field[i].setHorizontalAlignment(SwingConstants.CENTER);
+				add(input_field[i]);
+			}
+			for (int i = 0; i < w_length; i++) {
+				b[i] = new JLabel();
+				b[i].setFont(new Font("DungGeunMo", Font.PLAIN, 40));
+				b[i].setHorizontalAlignment(SwingConstants.CENTER);
+				add(b[i]);
+			}
+			for (int i = 0; i < w_length; i++) {
+				c[i] = new JLabel();
+				c[i].setFont(new Font("DungGeunMo", Font.PLAIN, 40));
+				c[i].setHorizontalAlignment(SwingConstants.CENTER);
+				add(c[i]);
+			}
+			for (int i = 0; i < w_length; i++) {
+				d[i] = new JLabel();
+				d[i].setFont(new Font("DungGeunMo", Font.PLAIN, 40));
+				d[i].setHorizontalAlignment(SwingConstants.CENTER);
+				add(d[i]);
+			}
+			for (int i = 0; i < w_length; i++) {
+				e[i] = new JLabel();
+				e[i].setFont(new Font("DungGeunMo", Font.PLAIN, 20));
+				e[i].setHorizontalAlignment(SwingConstants.CENTER);
+				add(e[i]);
+			}
+
+			// textfield 글자수 제한+자동포커스 이동
+			input_field[0].addKeyListener(new KeyAdapter() {
+				public void keyTyped(KeyEvent ke) {
+					JTextField src = (JTextField) ke.getSource();
+					if (src.getText().length() >= 1) {
+						ke.consume();
+						input_field[1].requestFocus();
+					}
+				}
+			});
+			input_field[1].addKeyListener(new KeyAdapter() {
+				public void keyTyped(KeyEvent ke) {
+					JTextField src = (JTextField) ke.getSource();
+					if (src.getText().length() >= 1) {
+						ke.consume();
+						input_field[2].requestFocus();
+					}
+				}
+			});
+			input_field[2].addKeyListener(new KeyAdapter() {
+				public void keyTyped(KeyEvent ke) {
+					JTextField src = (JTextField) ke.getSource();
+					if (src.getText().length() >= 1) {
+						ke.consume();
+						input_field[3].requestFocus();
+					}
+				}
+			});
+			input_field[3].addKeyListener(new KeyAdapter() {
+				public void keyTyped(KeyEvent ke) {
+					JTextField src = (JTextField) ke.getSource();
+					if (src.getText().length() >= 1) {
+						ke.consume();
+						input_field[4].requestFocus();
+					}
+				}
+			});
+			input_field[4].addKeyListener(new KeyAdapter() {
+				public void keyTyped(KeyEvent ke) {
+					JTextField src = (JTextField) ke.getSource();
+					if (src.getText().length() >= 1) {
+						ke.consume();
+						input_field[5].requestFocus();
+					}
+				}
+			});
+
+			// 엔터쳤을 때 액션
+			input_field[0].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					input_field[1].requestFocus();
+				}
+			});
+			input_field[1].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					input_field[2].requestFocus();
+				}
+			});
+			input_field[2].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					input_field[3].requestFocus();
+				}
+			});
+			input_field[3].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					input_field[4].requestFocus();
+				}
+			});
+			input_field[4].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					input_field[5].requestFocus();
+				}
+			});
+
+			// 단어 끝칸_글자수제한
+			input_field[w_length - 1].addKeyListener(new KeyAdapter() {
+				public void keyTyped(KeyEvent ke) {
+					JTextField src = (JTextField) ke.getSource();
+					if (src.getText().length() >= 1) {
+						ke.consume();
+						for (int i = 0; i < 6; i++) {
+							input1[i] = input_field[i].getText().charAt(0);
+						}
+						// 채점+다음 시도로 넘기기
+						t.interrupt();
+					}
+				}
+			});
+
+			// 단어 끝칸_엔터
+			input_field[w_length - 1].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					for (int i = 0; i < w_length; i++) {
+						input1[i] = input_field[i].getText().charAt(0);
+					}
+					// 채점+다음 시도로 넘기기
+					t.interrupt();
+				}
+			});
+
+			// 단어 끝칸_탭
+			input_field[w_length - 1].setFocusTraversalKeysEnabled(false);
+			input_field[w_length - 1].addKeyListener(new KeyAdapter() {
+				public void keyPressed(KeyEvent e) {
+					if (e.getKeyCode() == KeyEvent.VK_TAB) {
+						for (int i = 0; i < 6; i++) {
+							input1[i] = input_field[i].getText().charAt(0);
+						}
+						// 채점+다음 시도로 넘기기
+						t.interrupt();
+					}
+				}
+			});
+
+			setVisible(true);
+
+		} catch (Exception e) {
 			System.out.println("게임1 join");
 			e.printStackTrace();
 		}
-		
-		w_length=a;
-		setLayout(new GridLayout(5,w_length));		
-		
-		input1=new char[w_length];
-		grading=new boolean[w_length];
-		
-		for(int i=0;i<6;i++) {
-			input_field[i] = new JTextField();				
-		}
-		
-		for (int i = 0; i < w_length; i++) {
-			input_field[i].setFont(new Font("DungGeunMo",Font.PLAIN,20));
-			add(input_field[i]);
-		}
-		for (int i = 0; i < w_length; i++) {
-			b[i] = new JLabel();
-			input_field[i].setFont(new Font("DungGeunMo",Font.PLAIN,20));
-			add(b[i]);
-		}
-		for (int i = 0; i < w_length; i++) {
-			c[i] = new JLabel();
-			input_field[i].setFont(new Font("DungGeunMo",Font.PLAIN,20));
-			add(c[i]);
-		}
-		for (int i = 0; i < w_length; i++) {
-			d[i] = new JLabel();
-			input_field[i].setFont(new Font("DungGeunMo",Font.PLAIN,20));
-			add(d[i]);
-		}
-		for (int i = 0; i < w_length; i++) {
-			e[i] = new JLabel();
-			input_field[i].setFont(new Font("DungGeunMo",Font.PLAIN,20));
-			add(e[i]);
-		}
-
-		// textfield 글자수 제한+자동포커스 이동
-		input_field[0].addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent ke) {
-				JTextField src = (JTextField) ke.getSource();
-				if (src.getText().length() >= 1) {
-					ke.consume();
-					input_field[1].requestFocus();
-				}
-			}
-		});
-		input_field[1].addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent ke) {
-				JTextField src = (JTextField) ke.getSource();
-				if (src.getText().length() >= 1) {
-					ke.consume();
-					input_field[2].requestFocus();
-				}
-			}
-		});
-		input_field[2].addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent ke) {
-				JTextField src = (JTextField) ke.getSource();
-				if (src.getText().length() >= 1) {
-					ke.consume();
-					input_field[3].requestFocus();
-				}
-			}
-		});
-		input_field[3].addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent ke) {
-				JTextField src = (JTextField) ke.getSource();
-				if (src.getText().length() >= 1) {
-					ke.consume();
-					input_field[4].requestFocus();
-				}
-			}
-		});
-		input_field[4].addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent ke) {
-				JTextField src = (JTextField) ke.getSource();
-				if (src.getText().length() >= 1) {
-					ke.consume();
-					input_field[5].requestFocus();
-				}
-			}
-		});
-		
-		//엔터쳤을 때 액션
-		input_field[0].addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				input_field[1].requestFocus();
-			}
-		});
-		input_field[1].addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				input_field[2].requestFocus();
-			}
-		});
-		input_field[2].addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				input_field[3].requestFocus();
-			}
-		});
-		input_field[3].addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				input_field[4].requestFocus();
-			}
-		});
-		input_field[4].addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				input_field[5].requestFocus();
-			}
-		});
-		
-		
-		//단어 끝칸_글자수제한
-		input_field[w_length-1].addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent ke) {
-				JTextField src = (JTextField) ke.getSource();
-				if (src.getText().length() >= 1) {
-					ke.consume();
-					for(int i=0; i<6; i++) {
-						input1[i]=input_field[i].getText().charAt(0);
-					}					
-					//채점+다음 시도로 넘기기
-					t.interrupt();
-				}
-			}
-		});
-		
-		
-		//단어 끝칸_엔터
-		input_field[w_length-1].addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				for(int i=0; i<w_length; i++) {
-					input1[i]=input_field[i].getText().charAt(0);
-				}
-				//채점+다음 시도로 넘기기
-				t.interrupt();				
-			}
-		});
-		
-		//단어 끝칸_탭
-		input_field[w_length-1].setFocusTraversalKeysEnabled(false);
-		input_field[w_length-1].addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent e) {
-				if (e.getKeyCode() == KeyEvent.VK_TAB) {
-					for(int i=0; i<6; i++) {
-						input1[i]=input_field[i].getText().charAt(0);
-					}
-					//채점+다음 시도로 넘기기
-					t.interrupt();
-				}
-			}
-		});		
-
-		setVisible(true);		
 	}
 
 	public static void main(String[] args) {
--- Java_Project_Swing/src/game2.java
+++ Java_Project_Swing/src/game2.java
@@ -1,6 +1,5 @@
 import java.awt.*;
 import java.awt.event.*;
-
 import javax.swing.*;
 
 public class game2 extends JPanel {
@@ -25,169 +24,173 @@ public class game2 extends JPanel {
 		t.start();
 		try {
 			t.join();
-		} catch(Exception e) {
+
+			this.w_length = length;
+			setLayout(new GridLayout(5, w_length, 10, 10));
+
+			input2 = new char[w_length];
+			grading = new boolean[w_length];
+
+			for (int i = 0; i < 6; i++) {
+				input_field[i] = new JTextField();
+			}
+
+			for (int i = 0; i < w_length; i++) {
+				a[i] = new JLabel();
+				a[i].setFont(new Font("DungGeunMo", Font.PLAIN, 40));
+				a[i].setHorizontalAlignment(SwingConstants.CENTER);
+				a[i].setText(Character.toString(g1[i]));
+				if (g1_b[i]) {
+					a[i].setBackground(Color.green);
+				} else {
+					a[i].setBackground(Color.pink);
+				}
+				add(a[i]);
+			}
+			for (int i = 0; i < w_length; i++) {
+				add(input_field[i]);
+				input_field[i].setFont(new Font("DungGeunMo", Font.PLAIN, 40));
+				input_field[i].setHorizontalAlignment(SwingConstants.CENTER);
+			}
+			for (int i = 0; i < w_length; i++) {
+				c[i] = new JLabel();
+				c[i].setFont(new Font("DungGeunMo", Font.PLAIN, 40));
+				c[i].setHorizontalAlignment(SwingConstants.CENTER);
+				add(c[i]);
+			}
+			for (int i = 0; i < w_length; i++) {
+				d[i] = new JLabel();
+				d[i].setFont(new Font("DungGeunMo", Font.PLAIN, 40));
+				d[i].setHorizontalAlignment(SwingConstants.CENTER);
+				add(d[i]);
+			}
+			for (int i = 0; i < w_length; i++) {
+				e[i] = new JLabel();
+				e[i].setFont(new Font("DungGeunMo", Font.PLAIN, 40));
+				e[i].setHorizontalAlignment(SwingConstants.CENTER);
+				add(e[i]);
+			}
+
+			// textfield 글자수 제한+자동포커스 이동
+			input_field[0].addKeyListener(new KeyAdapter() {
+				public void keyTyped(KeyEvent ke) {
+					JTextField src = (JTextField) ke.getSource();
+					if (src.getText().length() >= 1) {
+						ke.consume();
+						input_field[1].requestFocus();
+					}
+				}
+			});
+			input_field[1].addKeyListener(new KeyAdapter() {
+				public void keyTyped(KeyEvent ke) {
+					JTextField src = (JTextField) ke.getSource();
+					if (src.getText().length() >= 1) {
+						ke.consume();
+						input_field[2].requestFocus();
+					}
+				}
+			});
+			input_field[2].addKeyListener(new KeyAdapter() {
+				public void keyTyped(KeyEvent ke) {
+					JTextField src = (JTextField) ke.getSource();
+					if (src.getText().length() >= 1) {
+						ke.consume();
+						input_field[3].requestFocus();
+					}
+				}
+			});
+			input_field[3].addKeyListener(new KeyAdapter() {
+				public void keyTyped(KeyEvent ke) {
+					JTextField src = (JTextField) ke.getSource();
+					if (src.getText().length() >= 1) {
+						ke.consume();
+						input_field[4].requestFocus();
+					}
+				}
+			});
+			input_field[4].addKeyListener(new KeyAdapter() {
+				public void keyTyped(KeyEvent ke) {
+					JTextField src = (JTextField) ke.getSource();
+					if (src.getText().length() >= 1) {
+						ke.consume();
+						input_field[5].requestFocus();
+					}
+				}
+			});
+
+			// 엔터쳤을 때 액션
+			input_field[0].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					input_field[1].requestFocus();
+				}
+			});
+			input_field[1].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					input_field[2].requestFocus();
+				}
+			});
+			input_field[2].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					input_field[3].requestFocus();
+				}
+			});
+			input_field[3].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					input_field[4].requestFocus();
+				}
+			});
+			input_field[4].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					input_field[5].requestFocus();
+				}
+			});
+
+			// 단어 끝칸_글자수제한
+			input_field[w_length - 1].addKeyListener(new KeyAdapter() {
+				public void keyTyped(KeyEvent ke) {
+					JTextField src = (JTextField) ke.getSource();
+					if (src.getText().length() >= 1) {
+						ke.consume();
+						for (int i = 0; i < 6; i++) {
+							input2[i] = input_field[i].getText().charAt(0);
+						}
+						// 채점+다음 시도로 넘기기
+						t.interrupt();
+					}
+				}
+			});
+
+			// 단어 끝칸_엔터
+			input_field[w_length - 1].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					for (int i = 0; i < w_length; i++) {
+						input2[i] = input_field[i].getText().charAt(0);
+					}
+					// 채점+다음 시도로 넘기기
+					t.interrupt();
+				}
+			});
+
+			// 단어 끝칸_탭
+			input_field[w_length - 1].setFocusTraversalKeysEnabled(false);
+			input_field[w_length - 1].addKeyListener(new KeyAdapter() {
+				public void keyPressed(KeyEvent e) {
+					if (e.getKeyCode() == KeyEvent.VK_TAB) {
+						for (int i = 0; i < 6; i++) {
+							input2[i] = input_field[i].getText().charAt(0);
+						}
+						// 채점+다음 시도로 넘기기
+						t.interrupt();
+					}
+				}
+			});
+
+			setVisible(true);
+
+		} catch (Exception e) {
 			System.out.println("게임2 join");
 			e.printStackTrace();
 		}
-		
-		this.w_length=length;
-		setLayout(new GridLayout(5,w_length));		
-		
-		input2=new char[w_length];
-		grading=new boolean[w_length];
-		
-		for(int i=0;i<6;i++) {
-			input_field[i] = new JTextField();				
-		}
-				
-		for (int i = 0; i < w_length; i++) {
-			a[i] = new JLabel();
-			input_field[i].setFont(new Font("DungGeunMo",Font.PLAIN,20));
-			a[i].setText(Character.toString(g1[i]));
-			if(g1_b[i]) {
-				a[i].setBackground(Color.green);
-			}
-			else {
-				a[i].setBackground(Color.pink);
-			}			
-			add(a[i]);
-		}
-		for (int i = 0; i < w_length; i++) {
-			add(input_field[i]);
-			input_field[i].setFont(new Font("DungGeunMo",Font.PLAIN,20));
-		}
-		for (int i = 0; i < w_length; i++) {
-			c[i] = new JLabel();
-			input_field[i].setFont(new Font("DungGeunMo",Font.PLAIN,20));
-			add(c[i]);
-		}
-		for (int i = 0; i < w_length; i++) {
-			d[i] = new JLabel();
-			input_field[i].setFont(new Font("DungGeunMo",Font.PLAIN,20));
-			add(d[i]);
-		}
-		for (int i = 0; i < w_length; i++) {
-			e[i] = new JLabel();
-			input_field[i].setFont(new Font("DungGeunMo",Font.PLAIN,20));
-			add(e[i]);
-		}
-
-		// textfield 글자수 제한+자동포커스 이동
-		input_field[0].addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent ke) {
-				JTextField src = (JTextField) ke.getSource();
-				if (src.getText().length() >= 1) {
-					ke.consume();
-					input_field[1].requestFocus();
-				}
-			}
-		});
-		input_field[1].addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent ke) {
-				JTextField src = (JTextField) ke.getSource();
-				if (src.getText().length() >= 1) {
-					ke.consume();
-					input_field[2].requestFocus();
-				}
-			}
-		});
-		input_field[2].addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent ke) {
-				JTextField src = (JTextField) ke.getSource();
-				if (src.getText().length() >= 1) {
-					ke.consume();
-					input_field[3].requestFocus();
-				}
-			}
-		});
-		input_field[3].addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent ke) {
-				JTextField src = (JTextField) ke.getSource();
-				if (src.getText().length() >= 1) {
-					ke.consume();
-					input_field[4].requestFocus();
-				}
-			}
-		});
-		input_field[4].addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent ke) {
-				JTextField src = (JTextField) ke.getSource();
-				if (src.getText().length() >= 1) {
-					ke.consume();
-					input_field[5].requestFocus();
-				}
-			}
-		});
-		
-		//엔터쳤을 때 액션
-		input_field[0].addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				input_field[1].requestFocus();
-			}
-		});
-		input_field[1].addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				input_field[2].requestFocus();
-			}
-		});
-		input_field[2].addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				input_field[3].requestFocus();
-			}
-		});
-		input_field[3].addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				input_field[4].requestFocus();
-			}
-		});
-		input_field[4].addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				input_field[5].requestFocus();
-			}
-		});
-		
-		
-		// 단어 끝칸_글자수제한
-		input_field[w_length - 1].addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent ke) {
-				JTextField src = (JTextField) ke.getSource();
-				if (src.getText().length() >= 1) {
-					ke.consume();
-					for (int i = 0; i < 6; i++) {
-						input2[i] = input_field[i].getText().charAt(0);
-					}
-					// 채점+다음 시도로 넘기기
-					t.interrupt();
-				}
-			}
-		});
-
-		// 단어 끝칸_엔터
-		input_field[w_length - 1].addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				for (int i = 0; i < w_length; i++) {
-					input2[i] = input_field[i].getText().charAt(0);
-				}
-				// 채점+다음 시도로 넘기기
-				t.interrupt();
-			}
-		});
-
-		// 단어 끝칸_탭
-		input_field[w_length - 1].setFocusTraversalKeysEnabled(false);
-		input_field[w_length - 1].addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent e) {
-				if (e.getKeyCode() == KeyEvent.VK_TAB) {
-					for (int i = 0; i < 6; i++) {
-						input2[i] = input_field[i].getText().charAt(0);
-					}
-					// 채점+다음 시도로 넘기기
-					t.interrupt();
-				}
-			}
-		});	
-
-		setVisible(true);		
 	}
 	public static void main(String[] args) {
 
