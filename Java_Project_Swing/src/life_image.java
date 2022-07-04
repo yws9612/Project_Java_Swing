7567e560c70282c90ca0cd78dd44e63c029e96ca
Seoyoung <seoyoung9710@gmail.com>
2022-07-04 15:19:39 +0900

    랭킹페이지 ui 적용 및 life 이미지 저장_서영_0704

--- Java_Project_Swing/src/life_image.java
+++ Java_Project_Swing/src/life_image.java
@@ -13,37 +13,37 @@ public class life_image {
 	public JLabel setLifeimg() {
 		switch(life) {
 		case 0:
-			img=new ImageIcon("");
+			img=new ImageIcon("img/heart_0.png");
 			life_label.setIcon(img);
 			return life_label;
 			
 		case 1:
-			img=new ImageIcon("");
+			img=new ImageIcon("img/heart_1.png");
 			life_label.setIcon(img);
 			return life_label;
 			
 		case 2:
-			img=new ImageIcon("");
+			img=new ImageIcon("img/heart_2.png");
 			life_label.setIcon(img);
 			return life_label;
 			
 		case 3:
-			img=new ImageIcon("");
+			img=new ImageIcon("img/heart_3.png");
 			life_label.setIcon(img);
 			return life_label;
 			
 		case 4:
-			img=new ImageIcon("");
+			img=new ImageIcon("img/heart_4.png");
 			life_label.setIcon(img);
 			return life_label;
 			
 		case 5:
-			img=new ImageIcon("");
+			img=new ImageIcon("img/heart_5.png");
 			life_label.setIcon(img);
 			return life_label;
 			
 		default:
-			img=new ImageIcon("");
+			img=new ImageIcon("img/heart_5.png");
 			life_label.setIcon(img);
 			return life_label;				
 		}
@@ -51,7 +51,7 @@ public class life_image {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
